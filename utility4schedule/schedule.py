4d8d4db808c7d7be7dd67e9559e62a74e27419d7
zhujidong <zhujidong@live.com>
2024-03-15 20:19:02 +0800

    my schedule

--- utility4schedule/schedule.py
+++ utility4schedule/schedule.py
@@ -1,5 +1,12 @@
 # -*- coding:utf_8 -*-
 
+'''
+The MIT License (MIT) 
+zhujidong 2021 Copyright(c), WITHOUT WARRANTY OF ANY KIND.
+
+'''
+
+
 import time
 from threading import Timer
 
@@ -24,64 +31,69 @@ class Schedule(object):
             handle:线程的句柄，供取消等使用
         '''
 
-
-    def reg_thread(self, name:str, fun:object, param:tuple, schedule:str, retry:tuple=(1,360),at_once:bool=False) -> None:
+    def reg_thread(self, name:str, fun:object, param:tuple, schedule:str, retry:tuple=(1,360),run_now:bool=False) -> None:
         '''
         注册一个计划任务， *要求补注册的任凭执行成功返为True,失败返回False
 
         :param:
             name:str,计划任务的名称
-            at_once:bool,注册计划任务后是否立即执行一次，再按按计划执行
+            run_now:bool,注册计划任务后是否立即执行一次，再按按计划执行
             其它参数相关信息，见__init__()。
         '''
         thread = {'fun':fun, 'param':param, 'schedule':schedule, 'retry':retry, 'errors':0, 'handle':None}
         self.threads[name] = thread
-        self._run_thread(name, at_once)
+        self._run_thread(name, run_now)
         return None
 
 
-    def _run_thread(self, name, run=True) -> None:
+    def _run_thread(self, name, run_now=True) -> None:
         '''
         调用Timer启动线程，每个线程就是一个等待下次计划时间运行的任务
 
         :param:
             name:str,要执行的任务名称
-            run:bool,为真时，立即执行不管计划，然后设置计划执行
-                     为假时，读取计划，设置计划时间执行
-                     *当注册线程按计划调用自身时，需为真，因为到了计划时间才调用的，否则永远不能执行计划任务
+            run_now:bool,为真时，立即运行任务而不管计划，然后设置下次计划时间运行
+                         为假时，读取计划，设置计划时间运行。
+                    *当此方法按计划调用自身时，通常为真，因为是到了计划时间才调用的，当然要运行任务
+                    *通常是给注册任务时使用，看是否当即运行一次，还是按计划时间运行
         '''
-        if run:
+        
+        #立即运行任务而不管计划，然后求出下次计划时间
+        if run_now:
             now = time.strftime("%m月%d日%H:%M", time.localtime(time.time()))
             print(F'\r\n----------------------------\r\n{now}：”{name}“任务启动...')
             
-            #执行计划的任务，成功返回真。       ***此处只将元组展开成位置参数给要执行的方法
+            #运行计划的任务，成功返回真。      *此处只将元组展开成位置参数给要执行的方法*
             rs = self.threads[name]['fun'](*self.threads[name]['param'])
-                                        
-            #生成下次的执行间隔
+                        
+            #调用方法生成到下次的执行任务的间隔秒数
             interval, nextdatetime = Schedule.get_interval(self.threads[name]['schedule'])
             nextdatetime = time.strftime("%m月%d日%H:%M", nextdatetime)
+
+            #根据任务执行的返回结果，调整下次执行间隔和提示信息
             if rs:
                 self.threads[name]['errors'] = 0
                 info = F'“{name}”任务执行完毕，下次计划于{nextdatetime}启动'
+            #任务执行中失败
             else:
-                #任务执行中失败
                 self.threads[name]['errors'] += 1
+                #没达到重试次数，调整下次执行的间隔为重试间隔
                 if self.threads[name]['errors']  <= self.threads[name]['retry'][0]:
-                    #调整下次执行的间隔为重试间隔
                     interval = self.threads[name]['retry'][1]
                     info = F'”{name}“任务执行失败，将在{interval/60}分种后重启...'
                 else:
                     info = F'”{name}“任务已失败{self.threads[name]["errors"]}次，不再重试，将在下次计划时间（{nextdatetime}）启动。'
                     self.threads[name]['errors'] = 0
+        #非立即运行的任务，调用得到时间间隔即可
         else:
-            #生成任务执行的时间间隔
             interval, nextdatetime = Schedule.get_interval(self.threads[name]['schedule'])
             nextdatetime = time.strftime("%m月%d日%H:%M", nextdatetime)
-            info = F'“{name}”任务注册完成，按计划将于{nextdatetime}首次执行'
+            info = F'“{name}”任务将按计划将于{nextdatetime}运行。'
 
+        #使用Timer,设置在计划时间段后，以线程方式运行本方法
         print(info,'\r\n----------------------------\r\n')
-        #按上面生成的时间间隔，设置本方法线程在计划时间后运行相应任务
         self.threads[name]['handle'] = Timer(interval, self._run_thread, args=(name, True))
+                                       #计划时间后才运行，所以通常给本方法传递true以立即运行任务
         self.threads[name]['handle'].start()
         return None
 
@@ -96,10 +108,23 @@ class Schedule(object):
         return None
 
 
+    def cancel_thread(self, name) -> None:
+        '''
+        取消某个计划的任务
+        '''
+        info = 0
+        if name in self.threads.keys():
+            self.threads[name]['handle'].cancel()
+            info = F'"{name}"任务已取消。'
+        else:
+            info = F'"{name}"任务不存在。'
+        return info
+
+
     @staticmethod
     def get_interval(schedule:list[tuple]) -> tuple:
         """
-        根据计划schedule（元组列表），计算到下次计划间隔的秒数。
+        根据计划schedule（元组列表），计算现在到下次计划间隔的秒数。
         
         :param:
             schedule：执行任务的计划表，是两个元素的元组列表。如
@@ -151,7 +176,7 @@ class Schedule(object):
         date_ = time.strftime("%Y-%m-%d", struct)
         time_ = time.strftime("%H:%M", struct)
 
-        #提取当天的计划列表 和 下个天的星期几和对应计划列表
+        #提取当天的计划列表 和 下一天任务是星期几和对应计划列表
         table = None
         for sche in schedule:
             if sche[0] == day_of_week:
@@ -168,36 +193,37 @@ class Schedule(object):
         
         #先在当天中查找下次计划，找到设置 next_stamp，否则置为 None
         next_stamp = None
+        #当天有计划，并且是定点执行的
         if table and ':' in table[0]:
-            #当天有计划，并且是 定点执行的
             for tb in table:
                 if tb > time_:
                     next_stamp = time.mktime(time.strptime(F'{date_} {tb}', '%Y-%m-%d %H:%M'))  
                     break
+        #当天有计划，是按时间间隔执行
         elif table:
-            #当天有计划，按时间间隔执行
             next_stamp = stamp + int(table[0])
             next_time = time.strftime("%H:%M", time.localtime(next_stamp))
+            #下个计划没到当天计划开始时间
             if next_time <=  table[1]:
-                #没到当天计划开始时间
                 next_stamp = time.mktime(time.strptime(F'{date_} {table[1]}', '%Y-%m-%d %H:%M'))  
+            #超计划的时间段了，不是今天的计划了
             elif next_time >= table[2]:
-                #不是今天的计划了
                 next_stamp =None
 
-        #当天没有计划或当天计划的时间之外,设置为下次那天的开始时间就可
+        #当天没有计划或当天计划的时间之外
         if next_stamp is None:
+            #设置计划为下一个天的开始时间就可
             if ':' in next_table[0]:
                 next_time = next_table[0]
             else:
                 next_time = next_table[1]
 
-            #下次与今天相距的天数
+            #下个计划天与今天相距的天数
             if next_day_of_week <= day_of_week:
                 next_day_of_week += 7
             days = next_day_of_week - day_of_week
 
-            # ***用今日、下次时间 生成的时间戳，还需要加上差的天数***
+            # 用 今日 和 下次计划时间 生成的时间戳，再加上相差天数的的秒数，即为下次计划的时间戳。
             next_stamp = time.mktime(time.strptime(F'{date_} {next_time}', '%Y-%m-%d %H:%M'))  
             next_stamp += days*24*3600
 
--- utility4schedule/test.py
+++ utility4schedule/test.py
@@ -5,32 +5,76 @@ import sys
 from configparser import ConfigParser
 
 import time
+import random
 from schedule import Schedule
 
-'''
-测试计划的格式
-'''
-config = ConfigParser()
-config.read( os.path.join(sys.path[0], "config.ini"), encoding='utf_8')
-my_schedule = config.items("my_schedule")
-print("从配置文件读出计划格式是:\r\n", my_schedule)
-'''
-    计划的格式是如下的元组列表,含义见 config.ini
-    [('6,7', '09:00, 14:00, 17:00'), 
-     ('1,5', '08:30, 10:30, 13:30, 15:30, 17:30, 20:00'), 
-     ('3', '3600, 06:00, 22:00') ]
-'''
+
+def read_config():
+    '''
+    测试计划的格式
+    '''
+    config = ConfigParser()
+    config.read( os.path.join(sys.path[0], "config.ini"), encoding='utf_8')
+    my_schedule = config.items("my_schedule")
+    print("从配置文件读出的计划格式是:\r\n", my_schedule)
+    '''
+        计划的格式是如下的元组列表,含义见 config.ini
+        [('6,7', '09:00, 14:00, 17:00'), 
+         ('1,5', '08:30, 10:30, 13:30, 15:30, 17:30, 20:00'), 
+         ('3', '3600, 06:00, 22:00') ]
+    '''
+    return my_schedule
 
 
-'''
-修改配置文件，测试下次计划时间
-'''
-interval, nextdatetime =  Schedule.get_interval(my_schedule)
-print("下次计划间隔：", interval)
-print("下次计划时间：", time.strftime("%m月%d日%H:%M", nextdatetime))
+def sche_time():
+    '''
+    测试生成计划下次运行时间
+    '''
+    interval, nextdatetime =  Schedule.get_interval(read_config())
+    print("\r\n测试调用方法生成下次计划运行时间：")
+    print("下次计划间隔：", interval)
+    print("下次计划时间：", time.strftime("%m月%d日%H:%M", nextdatetime))
+
 
 
 '''
 测试计划任任务执行
 '''
-def print_time(cur_time)
+def task1(arg):
+    rs = random.randint(0, 1)
+    print("这是task1，首次传入的参数:",arg)
+    print("任务将返回：",rs)
+    #根据返回值判断任务执行是否失败，决定是否重试
+    return rs
+
+def task2(arg):
+    rs = random.randint(0, 1)
+    print("\r\n这是任务二，首次传入的参数:",arg)
+    print("任务将返回：",rs)
+    return rs
+
+def sche():
+    sche = Schedule()
+    sche.reg_thread('task-one', task1, ('OK',), ([('5', '20, 01:00, 20:00'),]), (1,10),run_now=False)
+    sche.reg_thread('任务二', task2, ('二',), ([('5', '60, 01:00, 20:00'),]), (0,),True)
+
+    while True:
+        print ("输入q退出")
+        print ("输入任务名字取消任务")
+        str = input()
+        if str=='q':
+            sche.close_threads()
+            break
+        else:
+            rs = sche.cancel_thread(str)
+            print(rs)
+
+
+if __name__ == '__main__':
+
+    #read_config()
+
+    #sche_time()
+
+    sche()
+    
